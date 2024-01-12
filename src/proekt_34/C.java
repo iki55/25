f8482d0b4c04b8795c3dd6164b8dcc7a8a5a629c
iki55 <aleksovska.isidora@gmail.com>
2024-01-12 23:45:24 +0100

    first program

---
+++ src/proekt_34/C.java
@@ -0,0 +1,9 @@
+package proekt_34;
+
+public class C extends B { // SUBCLASS C
+	void triple() {
+	x=x+3; // metod - Zgolemi ja vrednosta na x so 3 x+=3
+	}
+}
+
+
---
+++ src/proekt_34/main.java
@@ -0,0 +1,16 @@
+package proekt_34;
+
+public class main {
+	public static void main(String[] args) {
+	B b = new B(); // Kreiraj objekt b - SUPERCLASS B
+	System.out.println("b.x:"+ b.returnIt()); // pecati 2
+	C c = new C(); // Kreiraj objekt c - SUBCLASS C
+	System.out.println("c.x:" + c.returnIt()); // pecati c.x 2
+	b.triple(); // promena na vrednosta b.x *=3
+	c.triple(); // promena na vrednosta c.x +=3
+	System.out.println("b.x posle b.triple():" + b.returnIt()); // pecati b.x =2*3 =
+	System.out.println("c.x posle c.triple():" + c.returnIt()); // pecati c.x =2+3 = 5
+	}
+	}
+
+
